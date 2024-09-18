15955abca16893f0534d633ae546647560c9c91e
toker-777 <toker_777@bk.ru>
2024-09-18 21:07:45 +0300

    add task one and two

---
+++ src/variables.java
@@ -0,0 +1,42 @@
+public class variables {
+    public static void main(String[] args) {
+        task1();
+        task2();
+
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
+
+        byte a = 120;
+        System.out.println(a);
+        short b = 31555;
+        System.out.println(b);
+        int c = 1351098;
+        System.out.println(c);
+        long d = -123456789;
+        System.out.println(d);
+        float e = 40f;
+        System.out.println(e);
+        double f = 306d;
+        System.out.println(f);
+    }
+    public static void task2 (){
+        System.out.println("Задача 2");
+
+        float a = 27.12F;
+        System.out.println(a);
+        long b = 987678965549L;
+        System.out.println(b);
+        double c = 2.786D;
+        System.out.println(c);
+        short d = 569;
+        System.out.println(d);
+        short e = -159;
+        System.out.println(e);
+        int f = 27897;
+        System.out.println(f);
+        byte g = 67;
+        System.out.println(g);
+
+    }
+}
